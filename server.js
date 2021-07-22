dc6d6e642a1467f171fff88854e8212375735256
Coco Puff <fahrija_wyroski@live.com>
2021-07-22 17:47:28 -0500

    adjust serverjs

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const path = require('path');
 const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
-const routes = require('./controllers');
+
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -36,7 +36,7 @@ app.set('view engine', 'handlebars');
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(routes);
+
 
 app.use(require('./controllers/'));
 
